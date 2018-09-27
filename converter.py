31e92031d39f5f25b67bb7e968c8a9c833d205bc
JaundiceJack <mcneilanjames@gmail.com>
2018-09-27 08:48:37 -0600

    separated input and display fields, added more comments

--- converter.py
+++ converter.py
@@ -4,6 +4,9 @@ from PyQt5.QtGui import QIntValidator, QDoubleValidator
 from PyQt5.QtCore import QCoreApplication
 from Interface import Ui_Dialog
 
+#convert the serving size or capsule weight to grams
+#
+
 #convert_to_double takes a string
 #if the string is a float, the text is returned as a float.
 #if the string was not a number, an exception is raised.
@@ -36,35 +39,42 @@ class Converter(QDialog):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        #create a QValidator to inhibit text entry
         self.double_validator = QDoubleValidator()
-        self.input = {'convert from': self.ui.fromCombo,
-                      'convert to': self.ui.toCombo,
-                      'serving': self.ui.servingLE,
-                      'arsenic entry': self.ui.asLE,
-                      'cadmium entry': self.ui.cdLE,
-                      'lead entry': self.ui.pbLE,
-                      'mercury entry': self.ui.hgLE,
-                      'arsenic result': self.ui.asL,
-                      'cadmium result': self.ui.cdL,
-                      'lead result': self.ui.pbL,
-                      'mercury result': self.ui.hgL}
-        self.restrict_input()
+        #define input fields
+        self.input = {'convert from': self.ui.fromCombo,  #given units
+                      'convert to': self.ui.toCombo,      #desired units
+                      'serving': self.ui.servingLE,       #serving/capsule weight
+                      'arsenic entry': self.ui.asLE,      #arsenic value
+                      'cadmium entry': self.ui.cdLE,      #cadmium value
+                      'lead entry': self.ui.pbLE,         #lead value
+                      'mercury entry': self.ui.hgLE}      #mercury value
+        #define display fields
+        self.display = {'arsenic result': self.ui.asL,      
+                        'cadmium result': self.ui.cdL,
+                        'lead result': self.ui.pbL,
+                        'mercury result': self.ui.hgL}
         self.arsenic = None
         self.cadmium = None
         self.lead = None
         self.mercury = None
+        #apply validators to the relevant input fields
+        self.restrict_input()        
+        #upon text entry, update the display to show the converted values
+        self.input['serving'].textChanged.connect(self.display_conversion)
         self.input['arsenic entry'].textChanged.connect(self.display_conversion)
         self.input['cadmium entry'].textChanged.connect(self.display_conversion)
         self.input['lead entry'].textChanged.connect(self.display_conversion)
         self.input['mercury entry'].textChanged.connect(self.display_conversion)
+        #place the UI onscreen
         self.show()
         
     def display_conversion(self):
         self.calculate_it()
-        self.input['arsenic result'].setText(self.arsenic)
-        self.input['cadmium result'].setText(self.cadmium)
-        self.input['lead result'].setText(self.lead)
-        self.input['mercury result'].setText(self.mercury)
+        self.display['arsenic result'].setText(self.arsenic)
+        self.display['cadmium result'].setText(self.cadmium)
+        self.display['lead result'].setText(self.lead)
+        self.display['mercury result'].setText(self.mercury)
 
     def calculate_it(self):
         self.arsenic = get_conversion(self.input['arsenic entry'],
