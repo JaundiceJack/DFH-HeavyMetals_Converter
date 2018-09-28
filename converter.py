ad89ec0ba25eca6765ece81c3a32a2e9bbbc66e9
JaundiceJack <mcneilanjames@gmail.com>
2018-09-28 12:36:01 -0600

    Cleaned up interface and code

--- converter.py
+++ converter.py
@@ -31,6 +31,12 @@ def get_conversion(field, serving):
     value2 = convert_to_double(serving.text())
     return str(value1 * (value2 * 0.001))
 
+#restrict_input takes a dict of line_edits and a QValidator. It applies the validator to each field.
+def restrict_input(input_fields, validator):
+    for name, field in input_fields.items():
+      field.setValidator(validator)
+
+
 class Converter(QDialog):
     """BasicDialog sets up a dialog box with the supplied user interface.
     Extracts basic functionality into an inheritable class"""    
@@ -39,59 +45,97 @@ class Converter(QDialog):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        #create a QValidator to inhibit text entry
-        self.double_validator = QDoubleValidator()
-        #define input fields
-        self.input = {'convert from': self.ui.fromCombo,  #given units
-                      'convert to': self.ui.toCombo,      #desired units
-                      'serving': self.ui.servingLE,       #serving/capsule weight
-                      'arsenic entry': self.ui.asLE,      #arsenic value
-                      'cadmium entry': self.ui.cdLE,      #cadmium value
-                      'lead entry': self.ui.pbLE,         #lead value
-                      'mercury entry': self.ui.hgLE}      #mercury value
-        #define display fields
-        self.display = {'arsenic result': self.ui.asL,      
-                        'cadmium result': self.ui.cdL,
-                        'lead result': self.ui.pbL,
-                        'mercury result': self.ui.hgL}
-        self.arsenic = None
-        self.cadmium = None
-        self.lead = None
-        self.mercury = None
-        #apply validators to the relevant input fields
-        self.restrict_input()        
-        #upon text entry, update the display to show the converted values
-        self.input['serving'].textChanged.connect(self.display_conversion)
-        self.input['arsenic entry'].textChanged.connect(self.display_conversion)
-        self.input['cadmium entry'].textChanged.connect(self.display_conversion)
-        self.input['lead entry'].textChanged.connect(self.display_conversion)
-        self.input['mercury entry'].textChanged.connect(self.display_conversion)
+        self.setup_interaction()
         #place the UI onscreen
         self.show()
-        
+
+    def setup_interaction(self):
+        self.setup_dropdowns()
+        self.setup_text_entry()
+        self.setup_displays()
+        self.validate_entry()
+        self.update_on_entry()
+     
+    def setup_dropdowns(self):
+      #define dropdown selectors
+      self.dropdown_fields = {
+        'from': self.ui.from_combo,             #given units
+        'to':   self.ui.to_combo,               #desired units
+        'weight_units': self.ui.weight_combo}   #serving size/tab/capsule weight units
+
+    def setup_text_entry(self):
+      #define text entry fields
+      self.text_entry_fields = {
+        'weight_entry': self.ui.weight_entry,       #serving/capsule weight value
+        'as_entry':     self.ui.given_as_entry,     #arsenic value
+        'cd_entry':     self.ui.given_cd_entry,     #cadmium value
+        'pb_entry':     self.ui.given_pb_entry,     #lead value
+        'hg_entry':     self.ui.given_hg_entry}     #mercury value
+
+    def setup_displays(self):
+      #define display fields
+      self.given_units = {
+        'as_unit': self.ui.given_as_unit,
+        'cd_unit': self.ui.given_cd_unit,
+        'pb_unit': self.ui.given_pb_unit,
+        'hg_unit': self.ui.given_hg_unit}
+      self.calc_units = {
+        'as_unit': self.ui.calc_as_unit,
+        'cd_unit': self.ui.calc_cd_unit,
+        'pb_unit': self.ui.calc_pb_unit,
+        'hg_unit': self.ui.calc_hg_unit}
+      self.calc_values = {
+        'as_value': self.ui.calc_as_value,
+        'cd_value': self.ui.calc_cd_value,
+        'pb_value': self.ui.calc_pb_value,
+        'hg_value': self.ui.calc_hg_value}
+
+    def validate_entry(self):
+      #create a QValidator to inhibit text entry
+      self.double_validator = QDoubleValidator()
+      #apply the validator to the relevant input fields
+      restrict_input(self.text_entry_fields, self.double_validator) 
+
+    def update_on_entry(self):
+      #upon text entry, update the display to show the converted values
+      for key, field in self.text_entry_fields.items():
+        field.textChanged.connect(self.display_conversion)
+      self.dropdown_fields['from'].currentIndexChanged.connect(self.update_given_units)
+      self.dropdown_fields['to'].currentIndexChanged.connect(self.update_calc_units)
+    
+    def update_given_units(self):
+      for key, field in self.given_units.items():
+        field.setText(self.dropdown_fields['from'].currentText())
+
+    def update_calc_units(self):
+      for key, field in self.calc_units.items():
+        field.setText(self.dropdown_fields['to'].currentText())
+
+
+
+
+
+
+
+
+    #calculate the converted values and set the display to the value  
     def display_conversion(self):
-        self.calculate_it()
-        self.display['arsenic result'].setText(self.arsenic)
-        self.display['cadmium result'].setText(self.cadmium)
-        self.display['lead result'].setText(self.lead)
-        self.display['mercury result'].setText(self.mercury)
+        self.calculate_conversion()
+        self.calc_values['as_value'].setText(self.arsenic)
+        self.calc_values['cd_value'].setText(self.cadmium)
+        self.calc_values['pb_value'].setText(self.lead)
+        self.calc_values['hg_value'].setText(self.mercury)
 
-    def calculate_it(self):
-        self.arsenic = get_conversion(self.input['arsenic entry'],
-                                      self.input['serving'])
-        self.cadmium = get_conversion(self.input['cadmium entry'],
-                                      self.input['serving'])
-        self.lead = get_conversion(self.input['lead entry'],
-                                   self.input['serving'])
-        self.mercury = get_conversion(self.input['mercury entry'],
-                                      self.input['serving'])
-
-    def restrict_input(self):
-        self.input['serving'].setValidator(self.double_validator) 
-        self.input['arsenic entry'].setValidator(self.double_validator) 
-        self.input['cadmium entry'].setValidator(self.double_validator) 
-        self.input['lead entry'].setValidator(self.double_validator) 
-        self.input['mercury entry'].setValidator(self.double_validator) 
+    #use the entered values to store
+    def calculate_conversion(self):
+        self.arsenic = get_conversion(self.text_entry_fields['arsenic entry'],
+                                      self.text_entry_fields['serving'])
+        self.cadmium = get_conversion(self.text_entry_fields['cadmium entry'],
+                                      self.text_entry_fields['serving'])
+        self.lead = get_conversion(self.text_entry_fields['lead entry'],
+                                   self.text_entry_fields['serving'])
+        self.mercury = get_conversion(self.text_entry_fields['mercury entry'],
+                                      self.text_entry_fields['serving'])
 
             
 if __name__ == '__main__':
